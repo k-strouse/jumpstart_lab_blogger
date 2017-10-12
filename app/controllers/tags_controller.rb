6e64540e15fda0f76cff6b86d37639300f9af5e2
Kyle Strouse <kstrouse09@gmail.com>
2017-10-12 15:10:37 -0500

    Added destroy method to tags controller & referential integrity

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,4 +6,13 @@ class TagsController < ApplicationController
 	def show
 		@tag = Tag.find(params[:id])
 	end
+
+	def destroy
+		@tag = Tag.find(params[:id])
+		@tag.destroy
+
+		flash.notice = "Article '#{@tag.name}' Deleted!"
+
+		redirect_to tags_path
+	end
 end
