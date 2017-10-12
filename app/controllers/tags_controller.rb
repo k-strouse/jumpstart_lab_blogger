92a2fd4db55e50fd80a959b24e573c65bafe2c5d
Kyle Strouse <kstrouse09@gmail.com>
2017-10-12 15:42:22 -0500

    Add image to show template so that it is visible to user

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -11,7 +11,7 @@ class TagsController < ApplicationController
 		@tag = Tag.find(params[:id])
 		@tag.destroy
 
-		flash.notice = "Article '#{@tag.name}' Deleted!"
+		flash.notice = "Tag '#{@tag.name}' Deleted!"
 
 		redirect_to tags_path
 	end
