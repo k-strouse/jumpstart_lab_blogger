bc3f7b77d20f76b2a3a1bb48beb72650998b54ee
Kyle Strouse <kstrouse09@gmail.com>
2017-10-12 16:43:09 -0500

    Migrate DB and update routes for sorcery

--- db/migrate/20171012212205_sorcery_core.rb
+++ db/migrate/20171012212205_sorcery_core.rb
@@ -3,8 +3,8 @@ class SorceryCore < ActiveRecord::Migration[5.1]
     create_table :authors do |t|
     	t.string :username,					:null => false
       t.string :email,            :null => false
-      t.string :crypted_password	:null =>false
-      t.string :salt							:null =>false
+      t.string :crypted_password,	:null => false
+      t.string :salt,							:null => false
 
       t.timestamps                
     end
